cc18cf6dd2b27863fc8e65dd983fef3f533c8431
VictorTamayoBriones <victortamayo509@gmail.com>
2021-09-17 00:27:21 -0500

    estructura

---
+++ src/theme.jsx
@@ -0,0 +1,8 @@
+const theme = {
+    rojo: '#591111',
+    naranja: '#E0A926',
+    naranjaClaro: '#F2B33D',
+    naranjaTransparente: 'rgba(224, 169, 38, 0.7)',
+    negroTransparente: 'rgba(0,0,0,0.5)',
+    sombra: 'rgba(0,0,0,0.3)'
+}
